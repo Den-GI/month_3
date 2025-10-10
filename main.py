06eb06b9c3d4e2597150475ef2ce33c6bd0fa826
Daniar <turgunbekovdaniar00@gmail.com>
2025-10-10 18:14:59 +0600

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from datetime import datetime
 
-# ввод имени
 name = input("Введите ваше имя: ")
 
-# получение текущего часа
 hour = datetime.now().hour
 
-# определяем приветствие
 if 6 <= hour < 12:
     greeting = "Доброе утро"
 elif 12 <= hour < 18:
@@ -16,5 +13,4 @@ elif 18 <= hour < 24:
 else:
     greeting = "Доброй ночи"
 
-# вывод результата
 print(f"{greeting}, {name}!")
