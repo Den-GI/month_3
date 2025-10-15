1f502b44ccb0b107b2288c24f8748d0bb4b3963c
Daniar <turgunbekovdaniar00@gmail.com>
2025-10-15 22:04:04 +0600

    hw4

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
+import flet as ft
 from datetime import datetime
 
-name = input("Введите ваше имя: ")
+def main(page: ft.Page):
+    page.title = "Приветствие"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    greeting_text = ft.Text("", size=20)
+    name_field = ft.TextField(label="Введите ваше имя")
+    
+    def update_greeting(e):
+        name = name_field.value
+        if name:
+            hour = datetime.now().hour
+            
+            if 6 <= hour < 12:
+                greeting = "Доброе утро"
+            elif 12 <= hour < 18:
+                greeting = "Добрый день"
+            elif 18 <= hour < 24:
+                greeting = "Добрый вечер"
+            else:
+                greeting = "Доброй ночи"
+            
+            greeting_text.spans = [
+                ft.TextSpan(text=f"{greeting}, "),
+                ft.TextSpan(
+                    text=name,
+                    style=ft.TextStyle(weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE)
+                ),
+                ft.TextSpan(text="!")
+            ]
+            
+        page.update()
+    
+    name_field.on_change = update_greeting
+    
+    page.add(
+        ft.Column([
+            name_field,
+            greeting_text
+        ])
+    )
 
-hour = datetime.now().hour
-
-if 6 <= hour < 12:
-    greeting = "Доброе утро"
-elif 12 <= hour < 18:
-    greeting = "Добрый день"
-elif 18 <= hour < 24:
-    greeting = "Добрый вечер"
-else:
-    greeting = "Доброй ночи"
-
-print(f"{greeting}, {name}!")
+ft.app(target=main)
